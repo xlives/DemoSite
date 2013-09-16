44f8a955e283440eb423d517806e606952844267
jefffischer <jfischer@broadleafcommerce.org>
2013-09-16 18:39:59 -0500

    support system event consumption

---
+++ core/src/main/resources/sql/load_scheduled_jobs.sql
@@ -0,0 +1,12 @@
+--
+-- The Archetype is configured with "hibernate.hbm2ddl.auto" value="create-drop" in "persistence.xml".
+--
+-- This will cause hibernate to populate the database when the application is started by processing the files that
+-- were configured in the hibernate.hbm2ddl.import_files property.
+--
+-- This file loads default scheduled jobs
+--
+
+-- Create a scheduled job to delete events that have been processed by all the nodes
+INSERT INTO BLC_SCHED_JOB (SCHED_JOB_ID, TYPE, NAME, GROUP_NAME, ENABLED, DATE_UPDATED, CRON_EXPRESSION) VALUES (-100, 'EVENT_PURGE', 'Event Purge', 'Purge', TRUE, CURRENT_TIMESTAMP, '0/30 * * * * ?');
+
